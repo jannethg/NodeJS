36a36bcc717c0f8aad15f01151c14a146f119e9d
Janneth Gutierrez <jgutier1@gmail.com>
2023-10-20 03:13:50 -0400

    Third Exercise - callback function

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
-
 const rect = require('./rectangle');
 
-function solveRect(l, w) {    
+function solveRect(l, w) {
     console.log(`Solving for rectangle with dimensions: ${l}, ${w}`);
 
-    if (l <= 0 || w <=0 ) {
-        console.log(`Rectangle dimension will be greater than zero, Received: ${l}, ${w}`);
-
-    } else {
-        console.log(`Area of the rectangle: ${rect.area(l, w)}` );
-        console.log(`Perimeter of the rectangle: ${rect.perimeter(l,w)}`);
-
-    }
+    rect(l, w, (err, rectangle) => {
+        if (err) {
+            console.log('ERROR:', err.message);
+        } else {
+            console.log(`Area of rectangle with dimensions ${l}, ${w} is: ${rectangle.area()}`);
+            console.log(`Perimeter of rectangle with dimensions ${l}, ${w} is: ${rectangle.perimeter()}`);
+        }
+    });
+    console.log('This statement is logged after the call to rect()');
 }
-
-
 
 solveRect(2, 4);
 solveRect(3, 5);
